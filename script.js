80d841c05b34f61432518bb70f504fd25a0e70c6
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2025-10-11 12:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	while(arr.length>1){
+		arr.sort((a,b) => a - b);
+		const small1 = arr[0];
+		const small2 = arr[1];
+
+		const sumOfSmall = small1+small2;
+
+		arr.splice(0,2);
+		arr.push(sumOfSmall);
+
+		
+		result += sumOfSmall; 
+	}
+	return result;
   
 }
 
